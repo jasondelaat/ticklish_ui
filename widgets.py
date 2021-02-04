600b1323f96ee9e6d217e4c1a02820edbd95d971
Jason DeLaat <jason.develops@gmail.com>
2021-02-04 17:38:23 -0500

    Fixed _WidgetFactory.options() to update rather tha overwrite the options dictionary.

--- widgets.py
+++ widgets.py
@@ -9,11 +9,12 @@ def Application(*rows):
 class _WidgetFactory:
     def __init__(self, widget_type):
         self.widget_type = widget_type
+        self.kwargs = {}
 
     def options(self, **kwargs):
-        self.kwargs = kwargs
+        for key in kwargs:
+            self.kwargs[key] = kwargs[key]
         return self
 
     def __call__(self, parent):
         return WIDGETS[self.widget_type](parent, **self.kwargs)
-        
