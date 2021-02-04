3ceab6187bac7f4bec515aedaa0edf93b61ca18e
Jason DeLaat <jason.develops@gmail.com>
2021-02-04 17:58:32 -0500

    Renamed EventStream to Stream.

--- events.py
+++ events.py
@@ -1,16 +1,16 @@
-class EventStream:
+class Stream:
     def __init__(self, predicate=lambda e: True, action=lambda e: e):
         self.predicate = predicate
         self.action = action
         self.children = []
 
     def filter(self, predicate):
-        stream = EventStream(predicate, lambda e: e)
+        stream = self.__class__(predicate, lambda e: e)
         self.children.append(stream)
         return stream
 
     def map(self, action):
-        stream = EventStream(lambda e: True, action)
+        stream = self.__class__(lambda e: True, action)
         self.children.append(stream)
         return stream
         
