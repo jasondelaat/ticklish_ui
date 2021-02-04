60df688a2e8703fe0b7f2fd600969e6db483dc01
Jason DeLaat <jason.develops@gmail.com>
2021-02-04 17:55:31 -0500

    Implemented EventStream.filter().

--- events.py
+++ events.py
@@ -2,3 +2,9 @@ class EventStream:
     def __init__(self, predicate=lambda e: True, action=lambda e: e):
         self.predicate = predicate
         self.action = action
+        self.children = []
+
+    def filter(self, predicate):
+        stream = EventStream(predicate, lambda e: e)
+        self.children.append(stream)
+        return stream
