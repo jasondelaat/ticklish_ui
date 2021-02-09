902923874d2c1455aab1f5cbff0f0ebaf954d8b0
Jason DeLaat <jason.develops@gmail.com>
2021-02-09 09:43:02 -0500

    Added Dropdown class.

--- ticklish_ui/widgets.py
+++ ticklish_ui/widgets.py
@@ -363,3 +363,42 @@ class Toplevel(ContainerFactory, tk.Toplevel):
         self.title(title)
         self.style = ttk.Style()
         self.style.theme_use('aqua')
+
+class Dropdown(WidgetFactory):
+    """Create a dropdown menu.
+
+    Dropdown is a ticklish addition which handles creating a menu
+    button and associated menu in single step.
+
+    """
+    def __init__(self, button_text, option_list):
+        """Initialize the Dropdown.
+
+        Arguments:
+            button_text - a string, the text displayed on the button
+                          before a selection is made.
+            option_list - a list of strings, the options to be
+                          displayed in the dropdown menu.
+
+        """
+        super().__init__(ttk.Menubutton)
+        self.button_text = button_text
+        self.option_list = option_list
+
+    def create_widget(self, parent):
+        dropdown = super().create_widget(parent)
+        variable = tk.StringVar(value=self.button_text)
+        dropdown['textvariable'] = variable
+
+        def display(value):
+            return lambda: variable.set(value)
+
+        menu = tk.Menu(dropdown)
+        for option in self.option_list:
+            menu.add_command(
+                label=option, command=display(option)
+            )
+        dropdown['menu'] = menu
+        dropdown.set = variable.set
+        dropdown.get = variable.get
+        return dropdown
