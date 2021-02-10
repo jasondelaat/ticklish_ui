63c4d11db32a7f06de56d9be87fde14b6df6f161
Jason DeLaat <jason.develops@gmail.com>
2021-02-10 09:04:30 -0500

    Changed how Dropdown works so it's just a Combobox set to readonly.

--- examples/dropdown.py
+++ examples/dropdown.py
@@ -5,7 +5,7 @@ app = Application(
     'Dropdown Example',
 
     # .row1
-    [Dropdown('Select one', [1, 2, 3, 4]).options(name='dd')],
+    [Dropdown('Select one', 1, 2, 3, 4).options(name='dd')],
 
     # .row2
     [Button('OK').options(name='ok'), CloseButton('Quit')]
@@ -22,13 +22,6 @@ def show_selection(e):
 (app.get_event_stream('<ButtonRelease-1>')
  .by_name('ok')
  .map(show_selection)
-)
-
-# When a selection is made the Dropdown generates a <<Selection>>
-# virtual event.
-(app.get_event_stream('<<Select>>')
- .by_name('dd')
- .map(lambda e: print(e.widget.get()))
 )
 
 app.mainloop()
--- ticklish_ui/widgets/dropdown.py
+++ ticklish_ui/widgets/dropdown.py
@@ -1,16 +1,14 @@
 """Defines the ticklish_ui Dropdown widget. """
-import tkinter as tk
-import tkinter.ttk as ttk
-from ticklish_ui.widgets.factories import WidgetFactory
+from ticklish_ui.widgets.combobox import Combobox
 
-class Dropdown(WidgetFactory):
+class Dropdown(Combobox):
     """Create a dropdown menu.
 
     Dropdown is a ticklish addition which handles creating a menu
     button and associated menu in single step.
 
     """
-    def __init__(self, button_text, option_list):
+    def __init__(self, *options):
         """Initialize the Dropdown.
 
         Arguments:
@@ -20,27 +18,11 @@ class Dropdown(WidgetFactory):
                           displayed in the dropdown menu.
 
         """
-        super().__init__(ttk.Menubutton)
-        self.button_text = button_text
-        self.option_list = option_list
+        super().__init__(*options)
+        self.initial_state = options[0]
 
     def create_widget(self, parent):
         dropdown = super().create_widget(parent)
-        variable = tk.StringVar(value=self.button_text)
-        dropdown['textvariable'] = variable
-        menu = tk.Menu(dropdown, name=f'{dropdown.winfo_name()}_menu')
-
-        for option in self.option_list:
-            menu.add_command(
-                label=option, command=_dd_update(dropdown, option)
-            )
-        dropdown['menu'] = menu
-        dropdown.set = variable.set
-        dropdown.get = variable.get
+        dropdown.state(['readonly'])
+        dropdown.set(self.initial_state)
         return dropdown
-
-def _dd_update(dropdown, value):
-    def command():
-        dropdown.set(value)
-        dropdown.event_generate('<<Select>>')
-    return command
